2f8cc1fec726089d8e85647ab9d11ff2a0e6281d
Ronnie Wang <ro_wsy@163.com>
2016-05-05 16:07:45 +0800

    refactor project, remove useless code

--- src/main/java/com/spider/config/WebSocketConfig.java
+++ src/main/java/com/spider/config/WebSocketConfig.java
@@ -25,8 +25,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/hello")
-                .withSockJS();
+        registry.addEndpoint("/hello").withSockJS();
     }
 
 }
