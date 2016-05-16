f651025b45958ef92d913649fffc2ac29418a919
Ronnie Wang <ro_wsy@163.com>
2016-05-16 17:44:39 +0800

    add java doc

--- src/main/java/com/spider/db/repository/NowgoalMatchStatisticRepository.java
+++ src/main/java/com/spider/db/repository/NowgoalMatchStatisticRepository.java
@@ -2,14 +2,10 @@ package com.spider.db.repository;
 
 import com.spider.db.entity.NowgoalMatchStatisticEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface NowgoalMatchStatisticRepository extends JpaRepository<NowgoalMatchStatisticEntity, Long> {
 
     List<NowgoalMatchStatisticEntity> findByMatchId(Long europeId);
-
-    @Transactional
-    void deleteByMatchId(long matchId);
 }
--- src/main/java/com/spider/db/repository/TCrawlerWin310HistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spider.db.repository;
-
-import com.spider.db.entity.TCrawlerWin310History;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TCrawlerWin310HistoryRepository extends JpaRepository<TCrawlerWin310History, Long> {
-
-}
