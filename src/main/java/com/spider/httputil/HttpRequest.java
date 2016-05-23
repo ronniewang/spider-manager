ca15a6aa18f06a988f6a0c4f056f83b562d93c80
Ronnie Wang <ro_wsy@163.com>
2016-05-23 17:44:01 +0800

    trust all certificates

--- src/main/java/com/spider/httputil/HttpRequest.java
+++ src/main/java/com/spider/httputil/HttpRequest.java
@@ -13,16 +13,31 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.ssl.SSLContextBuilder;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
 import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLException;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -133,7 +148,8 @@ public class HttpRequest {
 
     private HttpClient buildClient() {
 
-        return HttpClients.custom().setConnectionManager(connectionManager).build();
+        return getTrustAllHttpClient();
+//        return HttpClients.custom().setConnectionManager(connectionManager).build();
     }
 
     private <T> T buildResponse(HttpUriRequest request, Class<T> cls, ResponseHandlerSupport<T> responseHandler) throws HttpException {
@@ -183,5 +199,60 @@ public class HttpRequest {
                 (executeEnd - executeStart) + "ms", responseHandler.getStatusCode(), responseHandler.getStatusLine() != null ? responseHandler.getStatusLine().getReasonPhrase() : null,
                 responseHandler.getStatusLine(), responseHandler.getHeaders(), responseHandler.getRawResponse());
         logger.trace(msg);
+    }
+
+    private static CloseableHttpClient getTrustAllHttpClient() {
+
+        try {
+            SSLContextBuilder builder = SSLContexts.custom();
+            builder.loadTrustMaterial(null, new TrustStrategy() {
+
+                @Override
+                public boolean isTrusted(X509Certificate[] chain, String authType)
+                        throws CertificateException {
+
+                    return true;
+                }
+            });
+            SSLContext sslContext = builder.build();
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                    sslContext, new X509HostnameVerifier() {
+
+                @Override
+                public void verify(String host, SSLSocket ssl)
+                        throws IOException {
+
+                }
+
+                @Override
+                public void verify(String host, X509Certificate cert)
+                        throws SSLException {
+
+                }
+
+                @Override
+                public void verify(String host, String[] cns,
+                                   String[] subjectAlts) throws SSLException {
+
+                }
+
+                @Override
+                public boolean verify(String s, SSLSession sslSession) {
+
+                    return true;
+                }
+            });
+
+            Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder
+                    .<ConnectionSocketFactory>create().register("https", sslsf)
+                    .build();
+
+            PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(
+                    socketFactoryRegistry);
+            return HttpClients.custom()
+                    .setConnectionManager(cm).build();
+        } catch (Exception e) {
+            return HttpClients.createDefault();
+        }
     }
 }
